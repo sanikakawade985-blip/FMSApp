84f4013b53bf310fa52efa3e230d219f09038b91
DeeCoder <deeksha4.photography@gmail.com>
2026-02-18 11:45:50 +0530

    removed commented old code

--- src/screens/techDrawer/AIScreen.tsx
+++ src/screens/techDrawer/AIScreen.tsx
@@ -1,36 +1,3 @@
-// import { View, StyleSheet, Dimensions } from 'react-native';
-// import { COLORS } from '../../theme/colors';
-
-// const { height } = Dimensions.get('window');
-
-// export default function AIScreen() {
-//   return (
-//     <View style={styles.root}>
-//       <View style={styles.redBg} />
-//       <View style={styles.whiteSheet} />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   root: {
-//     flex: 1,
-//     backgroundColor: COLORS.primary,
-//   },
-//   redBg: {
-//     top: 0,
-//     height: height * 0.12,
-//     backgroundColor: COLORS.primary,
-//   },
-//   whiteSheet: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     borderTopLeftRadius: 28,
-//     borderTopRightRadius: 28,
-//     padding: 10,
-//   },
-// });
-
 import React from 'react';
 import {
   View,
